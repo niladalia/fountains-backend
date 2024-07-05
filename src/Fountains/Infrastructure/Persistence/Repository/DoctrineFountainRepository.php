1ed4236a23f974d427e4b9b017dc22ec96cfa40f
Carleslc <lazaro.costa.carles@gmail.com>
2024-07-05 04:01:38 +0200

    findByLocation + fix update with merge duplicates

--- src/Fountains/Infrastructure/Persistence/Repository/DoctrineFountainRepository.php
+++ src/Fountains/Infrastructure/Persistence/Repository/DoctrineFountainRepository.php
@@ -6,6 +6,8 @@ use App\Fountains\Domain\Fountain;
 use App\Fountains\Domain\FountainRepository;
 use App\Fountains\Domain\Fountains;
 use App\Fountains\Domain\ValueObject\FountainId;
+use App\Fountains\Domain\ValueObject\FountainLat;
+use App\Fountains\Domain\ValueObject\FountainLong;
 use App\Fountains\Domain\ValueObject\FountainProviderId;
 use App\Fountains\Domain\ValueObject\FountainProviderName;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -42,6 +44,16 @@ class DoctrineFountainRepository extends ServiceEntityRepository implements Foun
             ->where('fountains.provider_name.value = :provider_name AND fountains.provider_id.value = :provider_id')
             ->setParameter("provider_name", $providerName->getValue())
             ->setParameter("provider_id", $provider_id->getValue());
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+
+    public function findByLocation(FountainLat $lat, FountainLong $long): ?Fountain
+    {
+        $qb = $this->createQueryBuilder('fountains')
+            ->where('fountains.lat.value = :lat AND fountains.long.value = :long')
+            ->setParameter("lat", $lat->getValue())
+            ->setParameter("long", $long->getValue());
 
         return $qb->getQuery()->getOneOrNullResult();
     }
