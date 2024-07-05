75417010726994018c70c635e3e716eed521f36f
Carleslc <lazaro.costa.carles@gmail.com>
2024-07-05 04:01:38 +0200

    findByLocation + fix update with merge duplicates

---
+++ src/Fountains/Application/Find/FountainFinderByLocation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Fountains\Application\Find;
+
+use App\Fountains\Domain\Fountain;
+use App\Fountains\Domain\FountainRepository;
+use App\Fountains\Domain\ValueObject\FountainLat;
+use App\Fountains\Domain\ValueObject\FountainLong;
+
+class FountainFinderByLocation
+{
+    public function __construct(private FountainRepository $fountainRepository)
+    {
+        $this->fountainRepository = $fountainRepository;
+    }
+
+    public function __invoke(FountainLat $lat, FountainLong $long): ?Fountain
+    {
+        return $this->fountainRepository->findByLocation($lat, $long);
+    }
+}
