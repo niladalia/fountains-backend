3aa26c21df5174650782aff90a807d23db4334ac
Carleslc <lazaro.costa.carles@gmail.com>
2024-07-18 19:49:33 +0200

    FountainsGetController with optional bbox

--- src/Fountains/Infrastructure/Controllers/FountainsGetController.php
+++ src/Fountains/Infrastructure/Controllers/FountainsGetController.php
@@ -3,12 +3,13 @@
 namespace App\Fountains\Infrastructure\Controllers;
 
 use App\Fountains\Application\Find\FountainsFinder;
+use App\Fountains\Application\Find\Filter\BoundingBoxFilter;
 use App\Fountains\Application\Find\Filter\FountainsFilterBuilder;
 use App\Shared\Infrastructure\Symfony\ApiController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class FountainsGetController extends ApiController
 {
@@ -18,16 +19,46 @@ class FountainsGetController extends ApiController
 
         $this->validateRequest($queryParameters, $this->constraints());
 
-        $limit = $queryParameters['limit'] ?? null;
-        $offset = $queryParameters['offset'] ?? null;
+        if (empty($queryParameters)) {
+            $fountains = $fountainsFinder->findAll();
+        } else {
+            $limit = $queryParameters['limit'] ?? null;
+            $offset = $queryParameters['offset'] ?? null;
 
-        $fountainsFilter = (new FountainsFilterBuilder())
-            ->setLimit($limit)
-            ->setOffset($offset);
+            $fountainsFilter = (new FountainsFilterBuilder())
+                ->setLimit($limit)
+                ->setOffset($offset);
+            
+            $this->setBoundingBoxFilter($fountainsFilter, $queryParameters);
 
-        $fountains = $fountainsFinder->findByFilter($fountainsFilter);
+            $fountains = $fountainsFinder->findByFilter($fountainsFilter);
+        }
 
         return new JsonResponse($fountains->toArray(), Response::HTTP_OK);
+    }
+
+    private function setBoundingBoxFilter(FountainsFilterBuilder $fountainsFilter, array $queryParameters)
+    {
+        if ($this->withBoundingBoxParameters($queryParameters)) {
+            $this->validateRequest($queryParameters, $this->constraintsBoundingBox());
+
+            $fountainsFilter->setBoundingBoxFilter(
+                new BoundingBoxFilter(
+                    $queryParameters['south_lat'],
+                    $queryParameters['west_long'],
+                    $queryParameters['north_lat'],
+                    $queryParameters['east_long']
+                )
+            );
+        }
+    }
+
+    private function withBoundingBoxParameters(array $queryParameters): bool
+    {
+        return isset($queryParameters['south_lat'])
+            || isset($queryParameters['west_long'])
+            || isset($queryParameters['north_lat'])
+            || isset($queryParameters['east_long']);
     }
 
     private function constraints(): Assert\Collection
@@ -38,6 +69,21 @@ class FountainsGetController extends ApiController
                     'limit' => new Assert\Optional(new Assert\Type('int')),
                     'offset' => new Assert\Optional(new Assert\Type('int'))
                 ]
+            ]
+        );
+    }
+
+    private function constraintsBoundingBox(): Assert\Collection
+    {
+        return new Assert\Collection(
+            [
+                'fields' => [
+                    'south_lat' => new Assert\Type('float'),
+                    'west_long' => new Assert\Type('float'),
+                    'north_lat' => new Assert\Type('float'),
+                    'east_long' => new Assert\Type('float'),
+                ],
+                'allowExtraFields' => true
             ]
         );
     }
