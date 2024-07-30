dbc535b25d04cc469b6ca7f7d9de92c0a00fcf0c
Nil <niladalia.1@gmail.com>
2024-07-30 22:52:23 +0200

    added find fountain by id controller

---
+++ src/Fountains/Domain/Services/FountainFinder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Fountains\Domain\Services;
+
+use App\Fountains\Domain\Exceptions\FountainNotFound;
+use App\Fountains\Domain\Fountain;
+use App\Fountains\Domain\FountainRepository;
+use App\Fountains\Domain\ValueObject\FountainId;
+
+class FountainFinder
+{
+    private $fountainRepository;
+
+    public function __construct(FountainRepository $fountainRepository)
+    {
+        $this->fountainRepository = $fountainRepository;
+    }
+
+    public function __invoke(FountainId $id): Fountain
+    {
+
+        $fountain = $this->fountainRepository->findById($id);
+
+        if (!$fountain) {
+            FountainNotFound::throw($id->getValue());
+        }
+        
+        return $fountain;
+    }
+}
--- tests/Fountains/Application/Find/FountainFinderTest.php
+++ tests/Fountains/Application/Find/FountainFinderTest.php
@@ -2,8 +2,9 @@
 
 namespace App\Tests\Fountains\Application\Find;
 
-use App\Fountains\Application\Create\DTO\FountainRequest;
 use App\Fountains\Application\Find\FountainFinder;
+use App\Fountains\Domain\Exceptions\FountainNotFound;
+use App\Fountains\Domain\Services\FountainFinder as DomainFinder;
 use App\Tests\Fountains\Application\Find\DTO\FountainFinderRequestMother;
 use App\Tests\Fountains\Domain\FountainMother;
 use App\Tests\Fountains\Domain\ValueObject\FountainIdMother;
@@ -11,23 +12,48 @@ use App\Tests\Fountains\FountainsUnitTestCase;
 
 class FountainFinderTest extends FountainsUnitTestCase
 {
+    private DomainFinder $domainFountainFinder;
+    private FountainFinder $finder;
+
     public function setUp(): void
     {
-        parent::setUp();
-        //$this->finder = new FountainFinder($this->repository());
+        $this->domainFountainFinder = $this->createMock(DomainFinder::class);
+
+        $this->finder = new FountainFinder($this->domainFountainFinder);
     }
 
-    /*public function test_find_a_fountain_by_id(){
+    private function domainFinderShouldFind($id){
+        return $this->domainFountainFinder
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($id);
+    }
+
+    public function test_find_a_fountain_by_id(){
         $uuid = FountainIdMother::create();
 
         $fountainRequest = FountainFinderRequestMother::create($uuid->getValue());
         $fountain = FountainMother::create($uuid);
 
-        $this->shouldFind($fountain, $uuid);
+        $this->domainFinderShouldFind($uuid)->willReturn($fountain);
 
         $fountainFound =$this->finder->__invoke($fountainRequest);
 
         $this->assertEquals($fountainFound->name(), $fountain->name());
         $this->assertEquals($fountainFound->id(), $fountain->id());
-    }*/
+    }
+
+    public function test_should_throw_exception_if_not_found()
+    {
+        $uuid = FountainIdMother::create();
+
+        $fountainRequest = FountainFinderRequestMother::create($uuid->getValue());
+
+        $this->domainFinderShouldFind($uuid)->willThrowException(new FountainNotFound("Fountain {$uuid->getValue()} not found"));
+
+        $this->expectException(FountainNotFound::class);
+
+        $this->finder->__invoke($fountainRequest);
+    }
+
 }
