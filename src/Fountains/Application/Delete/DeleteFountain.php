32a299dd3a2658af407d70bf4095b854afa7e3eb
Nil <niladalia.1@gmail.com>
2024-11-04 08:54:43 +0100

    use omain finder in DeleteFountain

--- src/Fountains/Application/Delete/DeleteFountain.php
+++ src/Fountains/Application/Delete/DeleteFountain.php
@@ -3,7 +3,7 @@
 namespace App\Fountains\Application\Delete;
 
 use App\Fountains\Application\Delete\DTO\DeleteFountainRequest;
-use App\Fountains\Application\Find\FountainFinder;
+use App\Fountains\Domain\Services\FountainFinder;
 use App\Fountains\Domain\FountainRepository;
 use App\Fountains\Domain\ValueObject\FountainId;
 
